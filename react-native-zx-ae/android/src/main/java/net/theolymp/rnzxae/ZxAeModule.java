2f3b8674a3b98f19e027c93bbef7132e6e5e38b1
Robin Parker <robin.parker@theolymp.net>
2020-01-02 01:07:57 +0100

    iniz

---
+++ react-native-zx-ae/android/src/main/java/net/theolymp/rnzxae/ZxAeModule.java
@@ -0,0 +1,30 @@
+package net.theolymp.rnzxae;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+
+public class ZxAeModule extends ReactContextBaseJavaModule {
+
+    private final ReactApplicationContext reactContext;
+
+    public ZxAeModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
+
+    @Override
+    public String getName() {
+        return "ZxAe";
+    }
+
+    @ReactMethod
+    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
+        
+        
+
+        String result = "xyz";
+        callback.invoke(result);
+    }
+}
